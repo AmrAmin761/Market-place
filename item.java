0ef8b249a623579aca14c2eb93ae64e125c602ab
Mahmoud Khaled Mohamed <85359683+Mahmoud-khaled-m@users.noreply.github.com>
2022-06-27 20:32:05 +0200

    Create item.java

---
+++ item.java
@@ -0,0 +1,52 @@
+
+
+public class item {
+    private int ID;
+    private float Price;
+    private int Amount;
+    private String Brand;
+    private int OperationID;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public float getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int Price) {
+        this.Price = Price;
+    }
+
+    public int getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(int Amount) {
+        this.Amount = Amount;
+    }
+
+    public String getBrand() {
+        return Brand;
+    }
+
+    public void setBrand(String Brand) {
+        this.Brand = Brand;
+    }
+
+    public int getOperationID() {
+        return OperationID;
+    }
+
+    public void setOperationID(int OperationID) {
+        this.OperationID = OperationID;
+    }
+
+    
+    
+}
