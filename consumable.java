1e8e4d68d77134ad6798c5ec44a9ffff40cc9d5a
Mahmoud Khaled Mohamed <85359683+Mahmoud-khaled-m@users.noreply.github.com>
2022-06-27 19:23:51 +0200

    Create consumable.java

---
+++ consumable.java
@@ -0,0 +1,25 @@
+
+
+
+public class consumable {
+    private int ConsumableID;
+    private String  Production_date;           
+
+   
+    public int getConsumableID() {
+        return ConsumableID;
+    }
+
+    public void setConsumableID(int ConsumableID) {
+        this.ConsumableID = ConsumableID;
+    }
+
+    public String getProduction_date() {
+        return Production_date;
+    }
+
+    public void setProduction_date(String Production_date) {
+        this.Production_date = Production_date;
+    }
+    
+}
