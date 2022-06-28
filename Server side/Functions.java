db0cf6b4dcd54011d896e4af7f373d5de66bb9f3
Aly Mammoun <53837104+alymaamoun@users.noreply.github.com>
2022-06-28 04:08:47 +0200

    Update Functions.java

--- Server side/Functions.java
+++ Server side/Functions.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package serverversion;
 
 import java.io.*;
@@ -11,21 +7,22 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
-import static serverversion.ServerVersion.retrieve;
-import static serverversion.ServerVersion.update;
+
 
 
 
 
 public class Functions {
-    static class validations{
     
+    static class validations{
+    static boolean EnoughtAmount=true;
+    static Vector<Integer> products = new Vector<>();
     public static boolean Login(String email,String password) throws SQLException
     {
         boolean valid=true;
         
         String Query="Select * from person , user where UserID=ID and Email='"+email+"' and password= '"+password+"'";
-        ResultSet rs=retrieve(Query);
+        ResultSet rs=DatabaseManipulation.retrieve(Query);
         if(rs.next())
         {
             valid=true;
@@ -40,7 +37,7 @@ public class Functions {
     {
         boolean valid=true;
         String Query="Select * from  user where Email='"+email+"'" ;
-        ResultSet rs=retrieve(Query);
+        ResultSet rs=DatabaseManipulation.retrieve(Query);
         if(rs.next())
         {
             
@@ -51,7 +48,44 @@ public class Functions {
     }
     
     //public static boolean SignUp(String name , String email , String phone , String password)
-    
+    public static Vector CheckAndSetAmount(Vector<item> items) throws SQLException
+    {       
+             
+            int itemId;
+            int amountRequested;
+            int amountAv=0;
+            EnoughtAmount=true;
+            
+            for(int i=0;i<items.size();i++)
+            {   
+                
+                itemId=items.get(i).getID();
+                amountRequested=items.get(i).getAmount();
+                String Query="select amount ,brand from item where id='"+itemId+"'";
+                ResultSet rs=DatabaseManipulation.retrieve(Query);
+                if(rs.next())
+                {
+                    amountAv=Integer.valueOf(rs.getString(1));
+                    
+                    
+                }
+                if(amountAv>=amountRequested)
+                {
+                    
+                }
+                else
+                {
+                    items.get(i).setAmount(amountAv);  //max available
+                    products.add(itemId);
+                    EnoughtAmount=false;
+                    
+                }
+                 
+            }
+            return items;
+            
+            
+    }
     
     
     }
@@ -68,9 +102,9 @@ public class Functions {
                 String command="insert into person values";
                 String ID= String.valueOf(getMaxId()+1);
                 String Query=command+"('"+ID+"','"+name+"','"+password+"')";
-                update(Query);
+                DatabaseManipulation.update(Query);
                 Query="insert into user values('"+ID+"','"+phone+"','"+email+"')";
-                update(Query);
+                DatabaseManipulation.update(Query);
                 return true;
             }
         }
@@ -78,15 +112,123 @@ public class Functions {
         public static int getMaxId() throws SQLException
         {
             String Query="Select MAX(ID) from person" ;
-            ResultSet rs=retrieve(Query);
+            ResultSet rs=DatabaseManipulation.retrieve(Query);
             rs.next();
             int max=rs.getInt(1);
             return max;
         }
         
+        public static Vector retriveClothes() throws SQLException
+        {   
+            Vector<clothes> records = new Vector<clothes>();
+            String Query="Select * from item , clothes where ClotheID=ID" ;
+            ResultSet rs=DatabaseManipulation.retrieve(Query);
+            records.removeAllElements();
+             ResultSetMetaData RSMD = rs.getMetaData();
+                    while(rs.next())
+                    {       
+                        clothes clothe= new clothes();
+                        clothe.setID( Integer.valueOf(rs.getString(1)));
+                        clothe.setPrice( Integer.valueOf(rs.getString(2)));
+                        clothe.setAmount( Integer.valueOf(rs.getString(3)));
+                        clothe.setBrand(rs.getString(4));
+                        
+                        clothe.setSize(rs.getString(6));
+                        clothe.setGender(rs.getString(7));
+                        clothe.setCOLOR(rs.getString(8));
+                        clothe.setTYPE(rs.getString(9));
+                        System.out.println("row returned");
+                        records.add(clothe);
+                        
+                    }             
+                    
+        return records;
+        }
+        
+        public static Vector retriveConsumables() throws SQLException
+        {
+            Vector<consumables> records = new Vector<consumables>();
+            String Query="Select * from item , consumable where consumableID=ID" ;
+            ResultSet rs=DatabaseManipulation.retrieve(Query);
+            records.removeAllElements();
+             ResultSetMetaData RSMD = rs.getMetaData();
+                    while(rs.next())
+                    {       
+                        consumables consumable= new consumables();
+                        consumable.setID( Integer.valueOf(rs.getString(1)));
+                        consumable.setPrice( Integer.valueOf(rs.getString(2)));
+                        consumable.setAmount( Integer.valueOf(rs.getString(3)));
+                        consumable.setBrand(rs.getString(4));
+                        
+                        consumable.setProduction_date(rs.getString(6));
+                        System.out.println("row returned");
+                        records.add(consumable);    
+                    } 
+            return records;
         
         
     }
+        
+        public static boolean purchase(Vector<item> items) throws SQLException
+        {
+
+            validations.CheckAndSetAmount(items);
+            if((validations.EnoughtAmount))
+            {   
+                String Query;
+                for(int i=0;i<items.size();i++)
+                {
+                    Query="update item set Amount=Amount-"+items.get(i).getAmount()+" where ID= "+items.get(i).getID();
+                    DatabaseManipulation.update(Query);
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+    static class DatabaseManipulation
+    {
+            public static ResultSet retrieve(String Query) throws SQLException
+    {
+        int CC;   
+        String name;
+        Statement stmt=null;
+        ResultSet rs = null;
+
+        Connection con = null;
+
+        String str = null;
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/marketplace_database?zeroDateTimeBehavior=CONVERT_TO_NULL", "root","lolbiglol");
+        stmt = con.createStatement();
+        rs = stmt.executeQuery(Query);
+        ResultSetMetaData RSMD = rs.getMetaData();
+        CC = RSMD.getColumnCount();
+        name=RSMD.getColumnName(1);
+        
+        return rs;
     
-    
+    }
+    public static boolean update(String Query) throws SQLException
+    {
+        Statement stmt=null;
+        ResultSet rs = null;
+
+
+        Connection con = null;
+
+        String str = null;
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/marketplace_database?zeroDateTimeBehavior=CONVERT_TO_NULL", "root","lolbiglol");
+                
+        PreparedStatement add = con.prepareStatement(Query);
+        add.executeUpdate();
+        return true;
+    }
+    }
 }
+    
+    
+
